5b92635b870a2360f7d164b8a2d847981d742921
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-26 20:24:26 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,14 +1,12 @@
-class MP3_Importer 
-  
-  attr_accessor :path 
-  
+clclass MP3Importer
+  attr_reader :path, :files
+
   def initialize(path)
-    @path = path 
-  end 
-  
-  def files 
-  end 
-  
-  def import 
-  end 
-end 
+    @path = path
+    @files = Dir.entries(path).grep(/.*\.mp3/)
+  end
+
+  def import
+    @files.each {|file| Song.new_by_filename(file)}
+  end
+end
