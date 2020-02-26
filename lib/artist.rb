53399ebfc7706ef99a86455150d619fe570831b4
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-26 20:14:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,7 +23,7 @@ class Artist
   end 
   
   def self.find_or_create_by_name(name)
-    @@all.detect do |artist| artist.name == artist || @artist.new
+    @@all.find do |artist| artist == name || @artist.new
   end 
   end 
     
