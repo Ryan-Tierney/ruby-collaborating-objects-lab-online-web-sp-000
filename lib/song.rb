be3e3f47a868e5736bf4714e93682725b70aa3a7
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-26 19:05:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,10 +6,6 @@ class Song
     @name = name
   end
   
-  def artist 
-    @artists 
-  end 
-
   def self.new_by_filename(file_name)
     song = file_name.split(" - ")[1]
     artist = file_name.split(" - ")[0]
