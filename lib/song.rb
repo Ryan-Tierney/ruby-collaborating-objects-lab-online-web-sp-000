f03d363776ac05d6a9ef32d472805c021dda8813
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-26 20:31:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,31 +1,4 @@
 class Song
-  attr_accessor :name, :artist
-
-@@all = []
-  def initialize(name)
-    @name = name
-    @@all << self 
-  end
-
- def self.all 
-    @@all 
-  end 
-  
-  def self.new_by_filename(filename)
-    artist_name, song_name, extra = filename.split(" - ")
-    song = self.new(song_name)
-    artist = Artist.find_or_create_by_name(artist_name)
-    artist.add_song(song)
-  end
-  
-   def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
-  
-end 
-
-class Song
 
   attr_accessor :name, :artist
 
@@ -45,7 +18,10 @@ class Song
     new_song
   end
 
-
+ def self.all 
+    @@all 
+  end 
+  
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
