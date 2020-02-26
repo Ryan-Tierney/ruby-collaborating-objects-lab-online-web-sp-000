6348852264f66bbdfe1768f82163c1b2e4de3d0a
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-26 20:30:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,9 +29,13 @@ class Song
 
   attr_accessor :name, :artist
 
+@@all = []
+
   def initialize(name)
     @name = name
+    @@all << self 
   end
+
 
   def self.new_by_filename(file_name)
     song = file_name.split(" - ")[1]
